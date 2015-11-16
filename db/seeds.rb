3aac7b28a22af2e6ad1db1e677dfafa7bdc13967
HideakiSaito <hidepon3110@yahoo.co.jp>
2015-11-16 21:31:51 +0900

    #16 初期データ登録１　部位

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+Part.create([
+    {name: "胸"},
+    {name: "背中(上)"},
+    {name: "背中(下)"},
+    {name: "下半身"},
+    {name: "臀部"},
+    {name: "大腿四頭筋"},
+    {name: "ハムストリング"},
+    {name: "カーフ"},
+    {name: "肩（前）"},
+    {name: "肩（中）"},
+    {name: "肩（後）"},
+    {name: "上腕二頭筋"},
+    {name: "上腕三頭筋"},
+    {name: "前腕"},
+    {name: "腹筋（前）"},
+    {name: "腹筋（横）"}
+])
+
+
