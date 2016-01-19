6d21784bfaba7469714c1c4e2598a87668beac12
HideakiSaito <hidepon3110@yahoo.co.jp>
2016-01-20 00:10:31 +0900

    #31 herokuでアセットがうまく動かないので

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,9 +24,11 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+ config.serve_static_assets = true
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   # add H.saito
