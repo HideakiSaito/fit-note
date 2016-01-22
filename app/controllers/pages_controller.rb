024f4b00ef70601684b2fae9af2091cce183ea84
HideakiSaito <hidepon3110@yahoo.co.jp>
2016-01-22 23:49:03 +0900

    #31 考えてたのはできた。項目をちゃんとと新規をスーパー使って

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,17 +6,23 @@ class PagesController < InheritedResources::Base
 
   def create
 
-    @page = Page.new(page_params)
+    
+    create!
 
-    respond_to do |format|
-      if @page.save
-        format.html { redirect_to @page, notice: 'Page was successfully created.' }
-        format.json { render :show, status: :created, location: @page }
-      else
-        format.html { render :new }
-        format.json { render json: @page.errors, status: :unprocessable_entity }
-      end
-    end
+#     super
+
+#    @page = Page.new(page_params)
+#
+#    respond_to do |format|
+#      if @page.save
+#        format.html { redirect_to @page, notice: 'Page was successfully created.' }
+#        format.json { render :show, status: :created, location: @page }
+#      else
+#        format.html { render :new }
+#        format.json { render json: @page.errors, status: :unprocessable_entity }
+#      end
+#    end
+#
   end
 
   def update
@@ -24,9 +30,29 @@ class PagesController < InheritedResources::Base
     @page = Page.find(params[:id])
     @page.assign_attributes(page_params)
 
+    @new_lines = nil
+    #ここで、コピーするなら過去の、トレーニングを取得する
+    if  params[:page_copy] = 1
+      @copy_lines = Page.find(params[:copy_page][:id]).lines
+      @new_lines = @copy_lines.map do |copy_line| 
+        new_line = Line.new(item_id: copy_line.item_id,
+                           mode_id: copy_line.mode_id,
+                           weight_1: copy_line.weight_1,
+                           weight_2: copy_line.weight_2
+                           )
+        new_line.save
+        @page.lines << new_line
+      end
+    end
+
     respond_to do |format|
       if @page.save
-        format.html { redirect_to @page, notice: 'Page was successfully updated.' }
+        message = 'Page was successfully updated.'
+        unless @new_lines.empty?
+         message += 'Copy Lines!!!'
+        end
+        format.html { redirect_to @page,
+                      notice: message }
         format.json { render :show, status: :ok, location: @page }
       else
         format.html { render :edit }
@@ -39,5 +65,10 @@ class PagesController < InheritedResources::Base
   def page_params
     params.require(:page).permit(:date, :place, :start_time, :end_time, :memo, :image)
   end
+
+
+    def copy_line_params copy_line
+      copy_line.permit(:page_id, :no, :item_id, :mode_id, :weight_1, :reps_1, :memo_1, :weight_2, :reps_2, :memo_2, :weight_3, :reps_3, :memo_3, :weight_4, :reps_4, :memo_4, :weight_5, :reps_5, :memo_5, :weight_6, :reps_6, :memo_6, :weight_7, :reps_7, :memo_7, :weight_8, :reps_8, :memo_8, :weight_9, :reps_9, :memo_9, :weight_0, :reps_0, :memo_0)
+    end
 end
 
