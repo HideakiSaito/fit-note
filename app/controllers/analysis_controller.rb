be2160f375e97fca5de7fb1e1cc1e58972654d15
HideakiSaito <hidepon3110@yahoo.co.jp>
2016-02-03 20:50:00 +0900

    #31 in の順序関係あるか？

--- app/controllers/analysis_controller.rb
+++ app/controllers/analysis_controller.rb
@@ -3,7 +3,7 @@ class AnalysisController < ApplicationController
     #Big3 items id
     push_id = [1,2]
     pull_id = [4,5,6]
-    leg_id = [9,10,18]
+    leg_id = [9,18,10]
     #get data
 ##    dates = Page.order(:date).map(&:date)
     dates = Page.order(:date).map do |page|
