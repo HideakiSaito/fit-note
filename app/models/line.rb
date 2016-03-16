76a1c2711f4780a5fee5206cef9a21a9f2dffc29
HideakiSaito <hidepon3110@yahoo.co.jp>
2016-03-16 23:43:13 +0900

    #48 miyasuku

--- app/models/line.rb
+++ app/models/line.rb
@@ -10,6 +10,7 @@ class Line < ActiveRecord::Base
     order("page_id desc , no asc")
   end
   class <<self
+    #行NOを採番する
     def get_now_no(page_id) 
       l = Line.where("page_id=?",page_id).order(:no)
       return 1 if l.size == 0
@@ -25,52 +26,33 @@ class Line < ActiveRecord::Base
       end
     end
   end
-
+  #同一トレーニング場所で前回の同種目の記録を取得する
   def get_last_line
-    now = page.date
-    this = page.place
-    prev_page = Page.where("place = ? and date < ?",this,now).order("date desc").first
-    return nil unless prev_page
+    prev_page = Page.where("place = ? and date < ?",page.place ,page.date).order("date desc").first
+    return nil unless prev_page #前回のトレーニングなければnil
     prev_line = Line.joins(:page).where(pages:{id: prev_page.id}, item_id: item.id).first
-    prev_line
   end
-
+  #統計データ用にレップス合計を取得
   def get_sum_reps
     reps_1.to_i + reps_2.to_i + reps_3.to_i + reps_4.to_i + reps_5.to_i #nil.to_i => 0 を利用
   end
-
+  #前回、前々回とかの記録を簡単に見せるように
   def get_set_label(set)
     case set
-    when 1 then
-      w = weight_1 
-      r = reps_1
-      m = memo_1
-    when 2 
-      w = weight_2 
-      r = reps_2
-      m = memo_2
-    when 3 
-      w = weight_3 
-      r = reps_3
-      m = memo_3
-    when 4 
-      w = weight_4 
-      r = reps_4
-      m = memo_4
-    when 5 
-      w = weight_5 
-      r = reps_5
-      m = memo_5
+     when 1 then  w = weight_1; r = reps_1; m = memo_1;
+     when 2 then  w = weight_2; r = reps_2; m = memo_2;
+     when 3 then  w = weight_3; r = reps_3; m = memo_3;
+     when 4 then  w = weight_4; r = reps_4; m = memo_4;
+     when 5 then  w = weight_5; r = reps_5; m = memo_5;
     end
     w ||= ""; r ||= "";  m ||= ""; #nil => ""
-    #"前回＞" + w.to_s + "/" + r.to_s + " : " + m
     page.date.strftime("%m/%d(%a)") + "＞" + w.to_s + "/" + r.to_s + " : " + m
   end
-
+  #ラベル出力用
   def print_label
     page.page_label + '_' + item.name + '_' + no.to_s
   end
-
+  #このトレーニング日での最大挙上重量を計算して返す
   def this_max_reps
     line_max_reps(self)
   end
