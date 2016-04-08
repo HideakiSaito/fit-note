0d91d9c80ec846ff0fea6af95e56b7b329ad40e1
HideakiSaito <hidepon3110@yahoo.co.jp>
2016-04-08 22:01:11 +0900

    #58 diet とりあえず画面に出す

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,40 +1,55 @@
 class Page < ActiveRecord::Base
   has_many :lines,->{order("no ASC") } ,dependent: :destroy
   belongs_to :diet
- def diet_summaly
-   d = ""
-   d += diet.name if diet_id
-   d += "</br>"
-   d += "c:" + carbohydrate_1.to_s + "g, "
-   d += "f:" + fat_1.to_s + "g, "
-   d += "p:" + protein_1.to_s + "g, "
-   d += "v:" + vegetable_1.to_s + "g, "
-   d += diet_memo_1.to_s
-   d += "</br>"
-   d += "c:" + carbohydrate_2.to_s + "g, "
-   d += "f:" + fat_2.to_s + "g, "
-   d += "p:" + protein_2.to_s + "g, "
-   d += "v:" + vegetable_2.to_s + "g, "
-   d += diet_memo_2.to_s
-   d += "</br>"
-   d += "c:" + carbohydrate_3.to_s + "g, "
-   d += "f:" + fat_3.to_s + "g, "
-   d += "p:" + protein_3.to_s + "g, "
-   d += "v:" + vegetable_3.to_s + "g, "
-   d += diet_memo_3.to_s
-   d += "</br>"
-   d += "c:" + carbohydrate_4.to_s + "g, "
-   d += "f:" + fat_4.to_s + "g, "
-   d += "p:" + protein_4.to_s + "g, "
-   d += "v:" + vegetable_4.to_s + "g, "
-   d += diet_memo_4.to_s
-   d += "</br>"
-   d += "c:" + carbohydrate_5.to_s + "g, "
-   d += "f:" + fat_5.to_s + "g, "
-   d += "p:" + protein_5.to_s + "g, "
-   d += "v:" + vegetable_5.to_s + "g, "
-   d += diet_memo_5.to_s
- end
+  def diet_summaly
+    d = ""
+    d += diet.name if diet_id
+    d += "</br>"
+    if diet_memo_1
+      d += diet_memo_1.to_s
+      d += "</br>"
+      d += "c:" + carbohydrate_1.to_s + "g, "
+      d += "f:" + fat_1.to_s + "g, "
+      d += "p:" + protein_1.to_s + "g, "
+      d += "v:" + vegetable_1.to_s + "g, "
+      d += "</br>"
+    end
+    if diet_memo_2
+      d += diet_memo_2.to_s
+      d += "</br>"
+      d += "c:" + carbohydrate_2.to_s + "g, "
+      d += "f:" + fat_2.to_s + "g, "
+      d += "p:" + protein_2.to_s + "g, "
+      d += "v:" + vegetable_2.to_s + "g, "
+      d += "</br>"
+    end
+    if diet_memo_3
+      d += diet_memo_3.to_s
+      d += "</br>"
+      d += "c:" + carbohydrate_3.to_s + "g, "
+      d += "f:" + fat_3.to_s + "g, "
+      d += "p:" + protein_3.to_s + "g, "
+      d += "v:" + vegetable_3.to_s + "g, "
+      d += "</br>"
+    end
+    if diet_memo_4
+      d += diet_memo_4.to_s
+      d += "</br>"
+      d += "c:" + carbohydrate_4.to_s + "g, "
+      d += "f:" + fat_4.to_s + "g, "
+      d += "p:" + protein_4.to_s + "g, "
+      d += "v:" + vegetable_4.to_s + "g, "
+      d += "</br>"
+    end
+    if diet_memo_5
+      d += diet_memo_5.to_s
+      d += "</br>"
+      d += "c:" + carbohydrate_5.to_s + "g, "
+      d += "f:" + fat_5.to_s + "g, "
+      d += "p:" + protein_5.to_s + "g, "
+      d += "v:" + vegetable_5.to_s + "g, "
+    end
+  end
 
   def page_label
     id.to_s + "." + date.to_s + "_" + place
