e88545ecc9142c62aaed0ba7361c780d6a710849
HideakiSaito <hidepon3110@yahoo.co.jp>
2016-04-12 22:46:23 +0900

    #49 アナライズようのグラフOK

--- app/controllers/analysis_controller.rb
+++ app/controllers/analysis_controller.rb
@@ -16,19 +16,27 @@ class AnalysisController < ApplicationController
     render :index
   end
   def diet
-    #Big3 items id
-    push_id = [1,2]
-    pull_id = [4,18,5,6]
-    leg_id = [9] #19
-    analysis_initialize("ジム")#ChartUtilを利用
+    pages = Page.where("protein_1 > 0").order(:date)
+    dates = []
+    protein_data = []
+    fat_data = []
+    carbohydrate_data = []
+    vegetable_data = []
+    pages.each do |page|
+      dates << page.date.strftime("%y/%m/%d(%a)") 
+      #カロリー換算
+      protein_data << page.protein_sum * 4
+      fat_data  << page.fat_sum * 9
+      carbohydrate_data << page.carbohydrate_sum * 4
+      vegetable_data << page.vegetable_sum * 5 
+    end
     @chart = LazyHighCharts::HighChart.new('graph') do |f|
       f.title(text: '食事のバランス推移')
-      f.xAxis(categories: chart_dates)
-      f.series(name: '炭水化物', data: chart_data(push_id))
-      f.series(name: '脂肪', data: chart_data(pull_id))
-      f.series(name: 'たんぱく質', data: chart_data(leg_id))
-      #f.chart(type: "column")
-      #f.chart(type: "bar")
+      f.xAxis(categories: dates)
+      f.series(name: "野菜[/5g]" ,data: vegetable_data ,type: "column")
+      f.series(name: '炭水化物[kca]l', data: carbohydrate_data )
+      f.series(name: '脂肪[kcal]', data: fat_data)
+      f.series(name: 'たんぱく質[kcal]', data: protein_data)
       f.chart(type: "area")
       f.options[:plotOptions] = { area: { stacking: 'normal'} } 
     end
