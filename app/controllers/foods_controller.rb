40c8f39c182b18414bb4ebdbdb164b0ad9b87523
HideakiSaito <hidepon3110@yahoo.co.jp>
2016-05-05 21:06:40 +0900

    #69 food sortkey

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,4 +1,7 @@
 class FoodsController < InheritedResources::Base
+  def index
+    @foods = Food.all
+  end
   private
     def food_params
       params.require(:food).permit(:name, :diet_id, :calorie, :carbohydrate, :fat, :protein, :vegetable, :diet_memo,:food_category_id,:sort_key)
