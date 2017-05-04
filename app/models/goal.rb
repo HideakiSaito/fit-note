3a760fd0fa55acd2df0be5de15a4e8a2e9f0106f
hidepon3110 <hidepon3110@gmail.com>
2017-05-04 23:54:28 +0900

    ノート一覧の詳細モードは画像とコメントにして普通のブログ風にする #54

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,7 +1,14 @@
 class Goal < ActiveRecord::Base
+  include RepsCalculator
   belongs_to :user
   def memo_br
     memo.gsub(/\r\n|\r|\n/, "<br />")
+  end
+  def push_power
+    max_reps(push_val1,push_val2)
+  end
+  def pull_power
+    max_reps(pull_val1,pull_val2)
   end
   #スコープ
   default_scope -> do
